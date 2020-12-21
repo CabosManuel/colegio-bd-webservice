41e29dad2f5ebfaa4d30dc08803375e74d073c5d
CabosManuel <cabos.apolaya@gmail.com>
2020-12-21 00:09:49 -0500

    Nuevas consultas y correcciones

--- BD/bd_mpb_consultas_spring.sql
+++ BD/bd_mpb_consultas_spring.sql
@@ -8,13 +8,27 @@ select * from mallas;
 SELECT * FROM matriculas;
 select * from secciones;
 
+-- listar cursos de la malla más reciente de una estudiante
+select cu.curso_id, cu.nombre from cursos cu
+inner join mallas mll1 on mll1.curso_id = cu.curso_id
+inner join grados g on g.grado_id = mll1.grado_id
+inner join matriculas mtr on mtr.grado = g.grado_id
+inner join estudiantes e on e.dni_estudiante = mtr.dni_estudiante
+where e.dni_estudiante = '61933011' and
+	year(mtr.fecha) like year(now());
+
+-- seleccionar curso y notas por el DNI del estudiante
 select distinct c.nombre,n.nota1,n.nota2,n.nota3, year(n.fecha)
 from notas n 
 inner join cursos c on c.curso_id=n.curso_id 
 inner join estudiantes e on e.dni_estudiante = n.dni_estudiante 
-where year(n.fecha) = '2019' and e.dni_estudiante = '61933011';
+where year(n.fecha) = '2020' and e.dni_estudiante = '61933011';
 
-select distinct year(n.fecha) from notas n inner join estudiantes e on e.dni_estudiante = n.dni_estudiante where e.dni_estudiante = '61933011';
+-- selecionar los años del estudiante (tal vez se debería recuperar los años de la matrícula y no de las notas)
+select distinct year(n.fecha) 
+from notas n 
+inner join estudiantes e on e.dni_estudiante = n.dni_estudiante 
+where e.dni_estudiante = '61933011';
 
 -- listar nivel, grado y sesciones
 select s.seccion_id, n.nombre as niveles, concat(g.nombre,' ',s.nombre) as 'grado y seccion'
@@ -38,11 +52,11 @@ FROM matricula mat
 ORDER BY gra.id_grado;
 
 -- listar cursos por grado
-SELECT c.id_curso,c.nombre
-FROM malla m
-	INNER JOIN curso c
-	ON c.id_curso = m.id_curso
-WHERE m.id_grado = 14;
+SELECT c.curso_id,c.nombre
+FROM mallas m
+	INNER JOIN cursos c
+	ON c.curso_id = m.curso_id
+WHERE m.grado_id = 14;
 
 
 -- listar horario? (no actualizado)
