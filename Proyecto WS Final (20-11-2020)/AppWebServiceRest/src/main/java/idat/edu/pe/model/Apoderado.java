04fcc42bc3db17d6e1b9d841cb6d645eeaf64ad4
CabosManuel <cabos.apolaya@gmail.com>
2020-12-28 09:47:51 -0500

    Nuevo campo dirección para Apoderado

--- Proyecto WS Final (20-11-2020)/AppWebServiceRest/src/main/java/idat/edu/pe/model/Apoderado.java
+++ Proyecto WS Final (20-11-2020)/AppWebServiceRest/src/main/java/idat/edu/pe/model/Apoderado.java
@@ -11,42 +11,42 @@ import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name = "apoderados")
-public class Apoderado implements Serializable{
-
+public class Apoderado implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	private String dniApoderado;
-	
+
 	@Column
 	private String nombre;
-	
+
 	@Column
 	private String apellido;
 
 	@Column
 	private String correo;
-	
+
 	@Column
 	private String celular;
-	
+
 	@Column
 	private String pass;
-	
+
+	@Column
+	private String direccion;
+
 	@Column
 	private Boolean estado;
-	
+
 	@ManyToOne
-	@JoinColumn(name ="distrito_id", nullable = false, updatable = false,
-	foreignKey = @ForeignKey(foreignKeyDefinition = 
-	"foreign key(distrito_id) references distritos(distrito_id)"))
+	@JoinColumn(name = "distrito_id", nullable = false, updatable = false, foreignKey = @ForeignKey(foreignKeyDefinition = "foreign key(distrito_id) references distritos(distrito_id)"))
 	private Distrito distrito;
-	
-	@OneToMany(mappedBy = "apoderado", cascade = CascadeType.ALL ,fetch =FetchType.EAGER)
-    @Fetch(value = FetchMode.SUBSELECT)
+
+	@OneToMany(mappedBy = "apoderado", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@Fetch(value = FetchMode.SUBSELECT)
 	private Collection<Estudiante> itemsestudiante = new ArrayList<>();
-	
+
 	public Apoderado() {
 	}
 
@@ -55,13 +55,15 @@ public class Apoderado implements Serializable{
 	}
 
 	public Apoderado(String dniApoderado, String nombre, String apellido, String correo, String celular, String pass,
-			Boolean estado) {
+			String direccion, Boolean estado) {
+		super();
 		this.dniApoderado = dniApoderado;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.correo = correo;
 		this.celular = celular;
 		this.pass = pass;
+		this.direccion = direccion;
 		this.estado = estado;
 	}
 
@@ -137,5 +139,12 @@ public class Apoderado implements Serializable{
 		this.distrito = distrito;
 	}
 
-	
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
 }
