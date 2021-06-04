0bc73186fbf198e69b855ac05f2742a56d3499fa
CabosManuel <cabos.apolaya@gmail.com>
2021-06-04 18:58:35 -0500

    Eliminación service y repository de Malla y Distrito

--- workspace/api-restful-colegio/src/main/java/com/colegio/repository/MallaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.colegio.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.colegio.model.Malla;
-
-public interface MallaRepository extends CrudRepository<Malla, Integer> {
-
-}
