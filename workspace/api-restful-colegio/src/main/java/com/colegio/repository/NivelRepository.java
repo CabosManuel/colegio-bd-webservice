e784724b6452faf4685f35dd9fde6fe1bd65eb23
CabosManuel <cabos.apolaya@gmail.com>
2021-09-23 19:45:59 -0500

    Más eliminación de Grado, Nivel, Sección y otros
    Otros: Malla y Distrito

--- workspace/api-restful-colegio/src/main/java/com/colegio/repository/NivelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.colegio.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.colegio.model.Nivel;
-
-public interface NivelRepository extends CrudRepository<Nivel, Integer> {
-
-}
